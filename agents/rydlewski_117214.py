32d3445fca13e2a07a8d5ec86b80aa206efde8ab
Marek Rydlewski <marek.rydlewski@outlook.com>
2017-07-09 19:39:10 +0200

    Add sense before move

--- agents/rydlewski_117214.py
+++ agents/rydlewski_117214.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3)
+
 import random
 import numpy as np
 from action import Action
@@ -8,7 +10,7 @@ class Agent:
     def __init__(self, p, pj, pn, height, width, areaMap):
 
         self.times_moved = 0
-        self.direction = None
+        self.direction = Action.LEFT
 
         self.p = p
         self.p_small = (1 - p) / 4.0
@@ -16,18 +18,22 @@ class Agent:
         self.pn = pn    # p found jam if jam doesnt exists
         self.height = height
         self.width = width
-        self.map = np.array(areaMap)
+        self.map = np.empty((height, width), dtype='str')
         self.exit_coords = None
         self.jams_coords = []
+
+        for x in range(self.height):
+            for y in range(self.width):
+                self.map[x, y] = areaMap[x][y]
 
         # eqal prob
         self.hist = np.full((height, width), 1. / (height * width - 1))
 
         for (x, y), v in np.ndenumerate(self.hist):
-            if self.map[x, y] == "W":
+            if self.map[x][y] == "W":
                 self.hist[x, y] = 0
                 self.exit_coords = (x, y)
-            elif self.map[x, y] == "J":
+            elif self.map[x][y] == "J":
                 self.jams_coords.append((x, y))
 
         return
@@ -35,7 +41,27 @@ class Agent:
     # nie zmieniac naglowka metody, tutaj agent dokonuje obserwacji swiata
     # sensor przyjmuje wartosc True gdy agent ma uczucie stania w jamie
     def sense(self, sensor):
-        pass
+        self.__update_hist_after_sense(sensor)
+
+    def __update_hist_after_sense(self, sensor):
+        pj_actual = self.pj if sensor is True else 1.0 - self.pj
+        pn_actual = self.pn if sensor is True else 1.0 - self.pn
+        partial_sum = 0.0
+
+        for (x, y), v in np.ndenumerate(self.map):
+            if v == "J":
+                self.hist[x, y] *= pj_actual
+            elif v == ".":
+                self.hist[x, y] *= pn_actual
+            else:
+                self.hist[x, y] = 0
+            partial_sum += self.hist[x, y]
+
+        # normalization
+        self.hist = self.hist/partial_sum
+
+        # dup = np.sum(self.hist)
+        # print(dup)
 
     # nie zmieniac naglowka metody, tutaj agent decyduje w ktora strone sie ruszyc,
     # funkcja MUSI zwrocic jedna z wartosci [Action.UP, Action.DOWN, Action.LEFT, Action.RIGHT]
